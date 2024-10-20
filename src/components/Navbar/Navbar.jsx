ff2f726c481f60543de8dac2b98b058f34107b08
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-20 23:50:21 +0545

    sc

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" className="logo" />
